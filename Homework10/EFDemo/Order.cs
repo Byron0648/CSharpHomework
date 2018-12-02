5b50396f69d81cde71004539e07656b53730e16e
Byron0648 <1085436942@qq.com>
2018-12-02 19:59:32 +0800

    添加Homework10

---
+++ Homework10/EFDemo/Order.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF {
+    public class Order {
+        [Key]
+        public String Id { get; set; }
+        public String Customer { get; set; }
+        public String PhoneNum { get; set; }
+        public DateTime CreateTime { get; set; }
+        public List<OrderItem> Items { get; set; }
+        
+
+        public Order() {
+            Items = new List<OrderItem>();
+        }
+
+        public Order(string id, string customer,string phonenum, DateTime createTime, List<OrderItem> items) {
+            Id = id;
+            Customer = customer;
+            PhoneNum = phonenum;
+            CreateTime = createTime;
+            Items = items;
+
+        }
+    }
+}
